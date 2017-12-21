1255da6ca4c2e37028d11a28afe19dd6419b5321
zhangyadong1993@hotmail.com <zhangyadong1993@hotmail.com>
2017-12-21 22:37:07 +0800

    update EventBus

---
+++ Equal.DDD/Equal.DDD/EventBus/EventBus.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Castle.Windsor;
+using Castle.MicroKernel.Registration;
+
+namespace Equal.DDD
+{
+    /// <summary>
+    /// 事件总线
+    /// </summary>
+    public class EventBus : IEventBus
+    {
+        //public static EventBus Default => new EventBus();
+
+        /// <summary>
+        /// 默认事件总线实例
+        /// </summary>
+        public static EventBus Default { get; private set; }
+
+        static EventBus()
+        {
+            Default = new EventBus();
+        }
+
+        /// <summary>
+        /// Castle Windsor容器
+        /// </summary>
+        public IWindsorContainer IocContainer { get; private set; }
+
+        /// <summary>
+        /// 事件源对应事件处理器字典，线程安全集合
+        /// </summary>
+        private readonly ConcurrentDictionary<Type, List<Type>> _eventAndHandlerMapping;
+
+        private static object _lock = new object();
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public EventBus()
+        {
+            //IocContainer = new WindsorContainer();
+            IocContainer = DDD.IocContainer.GetContainer();
+            _eventAndHandlerMapping = new ConcurrentDictionary<Type, List<Type>>();
+        }
+
+        /// <summary>
+        /// 事件源对应事件处理器字典中指定事件源的事件处理器列表
+        /// </summary>
+        /// <param name="eventType"></param>
+        /// <returns></returns>
+        private List<Type> GetOrAddEventAndHandlerMapping(Type eventType)
+        {
+            return _eventAndHandlerMapping.GetOrAdd(eventType, type => new List<Type>());
+        }
+
+        /// <summary>
+        /// 注册事件处理器
+        /// </summary>
+        /// <param name="eventType">事件源类型</param>
+        /// <param name="handlerType">事件处理类型</param>
+        public void Register(Type eventType, Type handlerType)
+        {
+            //搜索具有指定名称的接口
+            var handlerInterface = handlerType.GetInterface("IEventHandler`1");
+            //若IocContainer不包含该组件，则注册事件处理器
+            if (!IocContainer.Kernel.HasComponent(handlerInterface))
+                IocContainer.Register(Component.For(handlerInterface, handlerType));
+
+            //添加事件源对应事件处理器字典中指定事件源的事件处理器列表
+            lock (_lock)
+                GetOrAddEventAndHandlerMapping(eventType).Add(handlerType);
+        }
+
+        /// <summary>
+        /// 注册事件处理器
+        /// </summary>
+        /// <typeparam name="TEventData"></typeparam>
+        /// <param name="eventHandler"></param>
+        public void Register<TEventData>(IEventHandler eventHandler)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register<TEventData>(Action<TEventData> action) where TEventData : IEventData
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterAllEventHandlerFromAssembly(Assembly assembly)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Trigger<TEventData>(TEventData eventData) where TEventData : IEventData
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Trigger<TEventData>(Type eventHandlerType, TEventData eventData) where TEventData : IEventData
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TriggerAsycn<TEventData>(Type eventHandlerType, TEventData eventData) where TEventData : IEventData
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TriggerAsync<TEventData>(TEventData eventData) where TEventData : IEventData
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnRegisterAll<TEventData>() where TEventData : IEventData
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Equal.DDD/Equal.DDD/EventBus/EventBusWithReflection.cs
+++ Equal.DDD/Equal.DDD/EventBus/EventBusWithReflection.cs
@@ -1,16 +1,135 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
-namespace Equal.DDD.EventBus
+namespace Equal.DDD
 {
+    /// <summary>
+    /// 事件总线，通过反射实现，不建议使用(有性能瓶颈隐患)
+    /// </summary>
     public class EventBusWithReflection
     {
+
+        //public static EventBusWithReflection Default => new EventBusWithReflection();
+
         /// <summary>
-        /// 单例
+        /// 默认事件总线实例
         /// </summary>
-        public static EventBusWithReflection Default => new EventBusWithReflection();
+        public static EventBusWithReflection Default { get; }
+
+        static EventBusWithReflection()
+        {
+            Default = new EventBusWithReflection();
+        }
+
+        /// <summary>
+        /// 事件源对应事件处理器集合字典，线程安全集合
+        /// </summary>
+        private readonly ConcurrentDictionary<Type, List<Type>> _eventAndHandlerMapping;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public EventBusWithReflection()
+        {
+            _eventAndHandlerMapping = new ConcurrentDictionary<Type, List<Type>>();
+            MapEventToHandler();
+        }
+
+        /// <summary>
+        /// 注册当前应用程序集中所有事件处理器，使用反射
+        /// </summary>
+        private void MapEventToHandler()
+        {
+            Assembly assembly = Assembly.GetEntryAssembly();
+            if (assembly == null)
+                return;
+
+            foreach (var type in assembly.GetTypes())
+            {
+                //判断是否实现了IEventHandler接口
+                if (typeof(IEventHandler).IsAssignableFrom(type))
+                {
+                    //获取该类实现的泛型接口
+                    Type handlerInterface = type.GetInterface("IEventHandler`1");
+                    if (handlerInterface != null)
+                    {
+                        //获取泛型接口指定的参数类型
+                        Type eventDataType = handlerInterface.GetGenericArguments()[0];
+
+                        if (_eventAndHandlerMapping.ContainsKey(eventDataType))
+                        {
+                            List<Type> handlerTypes = _eventAndHandlerMapping[eventDataType];
+                            handlerTypes.Add(type);
+                            _eventAndHandlerMapping[eventDataType] = handlerTypes;
+                        }
+                        else
+                        {
+                            var handlerTypes = new List<Type> { type };
+                            _eventAndHandlerMapping[eventDataType] = handlerTypes;
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 注册事件处理器
+        /// </summary>
+        /// <typeparam name="TEventData"></typeparam>
+        /// <param name="eventHandler"></param>
+        public void Register<TEventData>(Type eventHandler)
+        {
+            if (_eventAndHandlerMapping.Keys.Contains(typeof(TEventData)))
+            {
+                List<Type> handlerTypes = _eventAndHandlerMapping[typeof(TEventData)];
+                if (!handlerTypes.Contains(eventHandler))
+                {
+                    handlerTypes.Add(eventHandler);
+                    _eventAndHandlerMapping[typeof(TEventData)] = handlerTypes;
+                }
+            }
+            else
+                _eventAndHandlerMapping.TryAdd(typeof(TEventData), new List<Type>() { eventHandler });
+        }
+
+        /// <summary>
+        /// 取消事件注册
+        /// </summary>
+        /// <typeparam name="TEventData"></typeparam>
+        /// <param name="eventHandler"></param>
+        public void UnRegister<TEventData>(Type eventHandler)
+        {
+            List<Type> handlerTypes = _eventAndHandlerMapping[typeof(TEventData)];
+            if (handlerTypes.Contains(eventHandler))
+            {
+                handlerTypes.Remove(eventHandler);
+                _eventAndHandlerMapping[typeof(TEventData)] = handlerTypes;
+            }
+        }
+
+        /// <summary>
+        /// 触发事件
+        /// </summary>
+        /// <typeparam name="TEventData"></typeparam>
+        /// <param name="eventData"></param>
+        public void Trigger<TEventData>(TEventData eventData) where TEventData : IEventData
+        {
+            List<Type> handlers = _eventAndHandlerMapping[typeof(TEventData)];
+
+            if (handlers != null && handlers.Count > 0)
+            {
+                foreach (var handler in handlers)
+                {
+                    MethodInfo methodInfo = handler.GetMethod("HandleEvent");
+                    if (methodInfo != null)
+                    {
+                        object obj = Activator.CreateInstance(handler);
+                        methodInfo.Invoke(obj, new object[] { eventData });
+                    }
+                }
+            }
+        }
     }
 }
